67214df922b6ac551235bcefee2982ba54943943
Andrin <andrin.bertschi@gmail.com>
2017-06-07 15:24:15 +0200

    prepare for alpha release

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 install_reqs = parse_requirements('./requirements.txt', session='hack')
 reqs = [str(ir.req) for ir in install_reqs]
 
+
 def read(*parts):
     # intentionally *not* adding an encoding option to open
     return codecs.open(os.path.join(here, *parts), 'r').read()
@@ -30,12 +31,25 @@ setup(
     license='Apache Software License',
     author='Andrin Bertschi',
     install_requires=reqs,
-    description='A wrapper around the postcard creator API of swiss post',
+    description='A python wrapper around the Rest API of the Swiss Postcard creator',
     packages=['postcard_creator_wrapper'],
-    include_package_data=True,
+    #include_package_data=True,
+    #package_data={'postcard_creator_wrapper': ['*.svg']},
     platforms='any',
     classifiers=[
-        ],
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: Apache License',
+        'Programming Language :: Python :: 3.6',
+    ],
     extras_require={
-      }
+    }
 )
