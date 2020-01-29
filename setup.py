b6b46f2ab86d72a8be4021e2ce8f0fbc3ec3c4bc
Andrin Bertschi <abertschi@abertschi.ch>
2020-01-29 15:37:44 +0100

    update compatibility issue with python 3.8

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup
-from pip.req import parse_requirements
 import codecs
 import os
 import re
